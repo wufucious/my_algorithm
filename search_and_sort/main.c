834c9e81ce66cb12aa9a621babdfad4fc94c1d5e
wufucious <wonderfulwyq@163.com>
2017-09-18 21:47:54 +0800

    add quick sort

--- search_and_sort/main.c
+++ search_and_sort/main.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
 
-int insert_sort(int *a, int n)
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int insertSort(int *a, int n)
 {
     int i,j;
     for(i=1; i < n; i++){
@@ -20,7 +70,7 @@ int insert_sort(int *a, int n)
 
 int main(int argc, char *argv[])
 {
-    int a[5] = {5,2,3,4,1};
+    int a[5] = {5,4,3,2,1};
 
     printf("The unsorted arrary is");
     for(int n=0; n < 5; n++){
@@ -28,7 +78,9 @@ int main(int argc, char *argv[])
     }
     printf("\n");
 
-    insert_sort(a,5);
+//    insertSort(a,5);
+    quickSort(a,0,4);
+
 
     printf("The sorted arrary is");
     for(int n=0; n < 5; n++){
